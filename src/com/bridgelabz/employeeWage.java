66212f3a0699119fa22ef54d8dc1a2e4da8819a6
akshayzagad <akshayzagade62@gmail.com.com>
2022-09-30 18:58:30 +0530

    Added: Welcome Employee Wage

---
+++ src/com/bridgelabz/employeeWage.java
@@ -0,0 +1,8 @@
+package com.bridgelabz;
+
+public class employeeWage {
+    public static void main(String[] args) {
+        System.out.println("Welcome to employee wage compitation problem ");
+
+    }
+}

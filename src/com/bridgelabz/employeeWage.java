9fc0caed6134d3684ce3a9a9abe902a5864090c1
akshayzagad <akshayzagade62@gmail.com.com>
2022-09-30 23:47:20 +0530

    Added : Calculate Employee Wage Using Swich Case

--- src/com/bridgelabz/employeeWage.java
+++ src/com/bridgelabz/employeeWage.java
@@ -4,18 +4,24 @@ public class employeeWage {
     static final int Wage_Per_Hour=20;
     static final int Full_Time_Hour=8;
     static final int Part_Time_Hour=4;
+    static final int Is_Part_Time_Hour=1;
+    static final int Is_Full_Time_Hour=2;
     public static void main(String[] args) {
         int employeeAttendance=(int)Math.floor(Math.random()*10)%3;
         System.out.println(employeeAttendance);
         int dailyWage=0;
-        if (employeeAttendance==1){
-            System.out.println("Employee is Present");
-            dailyWage=Wage_Per_Hour*Full_Time_Hour;
-        } else if (employeeAttendance==2) {
-            dailyWage=Wage_Per_Hour*Part_Time_Hour;
+        switch (employeeAttendance){
+            case Is_Full_Time_Hour :
+                System.out.println("Employee is Present");
+                dailyWage=Wage_Per_Hour*Full_Time_Hour;
+                break;
+            case Is_Part_Time_Hour:
+                dailyWage=Wage_Per_Hour*Part_Time_Hour;
+                System.out.println("Employee is Part Time");
+                break;
+            default:
+                System.out.println("Absent");
 
-        } else {
-            System.out.println("Employee is absent");
         }
         System.out.println("daily Wage==>"+dailyWage);
     }

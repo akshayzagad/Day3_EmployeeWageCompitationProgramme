400becf7ef730883f7d9bf43ca68f7f74945d87a
akshayzagad <akshayzagade62@gmail.com.com>
2022-09-30 19:08:19 +0530

    Added: Find Out Employee Present or Absent

--- src/com/bridgelabz/employeeWage.java
+++ src/com/bridgelabz/employeeWage.java
@@ -2,7 +2,13 @@ package com.bridgelabz;
 
 public class employeeWage {
     public static void main(String[] args) {
-        System.out.println("Welcome to employee wage compitation problem ");
-
+        int employeeAttendance=(int)Math.round(Math.random());
+        System.out.println(employeeAttendance);
+        if (employeeAttendance==1){
+            System.out.println("Employee is Present");
+        }
+        else {
+            System.out.println("Employee is absent");
+        }
     }
 }

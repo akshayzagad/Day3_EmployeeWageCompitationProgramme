35a010d0754331beb57f005a30c8eec3e6a12071
akshayzagad <akshayzagade62@gmail.com.com>
2022-10-13 20:12:44 +0530

    Added; Rewrite Program Using Methods

--- src/com/bridgelabz/employeeWage.java
+++ src/com/bridgelabz/employeeWage.java
@@ -1,20 +1,26 @@
 package com.bridgelabz;
 
 public class employeeWage {
-    static final int Wage_Per_Hour=20;
-    static final int Full_Time_Hour=8;
-    static final int Part_Time_Hour=4;
-    static final int Is_Part_Time_Hour=1;
-    static final int Is_Full_Time_Hour=2;
-    static final int Working_Days_Per_Month=20;
-    public static void main(String[] args) {
-        int day=1;
-        int totalWage=0;
-        while(day<=Working_Days_Per_Month) {
+    static final int Wage_Per_Hour = 20;
+    static final int Full_Time_Hour = 8;
+    static final int Part_Time_Hour = 4;
+    static final int Is_Part_Time_Hour = 1;
+    static final int Is_Full_Time_Hour = 2;
+    static final int Working_Days_Per_Month = 20;
+
+    static int employeeAttendance() {
+        int employeeAttendance = (int) Math.floor(Math.random() * 10) % 3;
+        return employeeAttendance;
+    }
+
+    static int switchCase() {
+        int day = 1;
+        int totalWage = 0;
+        while (day <= Working_Days_Per_Month) {
             int dailyWage = 0;
-            int employeeAttendance = (int) Math.floor(Math.random() * 10) % 3;
-            System.out.println(employeeAttendance);
-            switch (employeeAttendance) {
+            int employeeAttendanceCheck = employeeAttendance();
+            System.out.println(employeeAttendanceCheck);
+            switch (employeeAttendanceCheck) {
                 case Is_Full_Time_Hour:
                     System.out.println("Employee is Present");
                     dailyWage = Wage_Per_Hour * Full_Time_Hour;
@@ -26,10 +32,19 @@ public class employeeWage {
                 default:
                     System.out.println("Absent");
             }
-            totalWage=totalWage + dailyWage;
+            totalWage = totalWage + dailyWage;
             System.out.println("daily Wage==>" + day + "day =>" + dailyWage);
             day++;
         }
-        System.out.println("Total Wage => "+totalWage);
+        System.out.println("Total Wage => " + totalWage);
+        return totalWage;
     }
-}
+
+
+    public static void main(String[] args) {
+       int totalWage=switchCase();
+        System.out.println(totalWage);
+        }
+
+    }
+
